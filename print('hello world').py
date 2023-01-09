2319f831036c6c9f676095f56977764d3f10611b
akshitha123hub <121024936+akshitha123hub@users.noreply.github.com>
2023-01-09 13:46:05 +0530

    new code added

--- print('hello world').py
+++ print('hello world').py
@@ -4,3 +4,11 @@ print('hello world')
 ##new change
 print("new change")
 print("new line")
+
+# haii i'm akshitha
+#code for checking odd or even
+num=int(input("enter a number: ")
+if(num%2==0):
+        print(" you entered an even number")
+else:
+        print(" you entered an odd number")
